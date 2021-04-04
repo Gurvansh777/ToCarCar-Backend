98d8d69981fd4ada5f16045d94bf4804a70a8b54
harman <harmansingh088@gmail.com>
2021-04-04 22:32:14 +0000

    Minor API Helper changes

--- ToCarCar/apiHelper.py
+++ ToCarCar/apiHelper.py
@@ -13,8 +13,6 @@ client = MongoClient(MONGO_CLIENT_URL)
 db = client.tocarcar #database
 
 
-#Headers -  Content-Type: application/json
-#           objectName: tasks
 @apiHelper_bp.route('/api/getallrecords', methods = ['GET', 'POST'])
 def getAllRecords():
     objectName = request.headers["objectName"]
@@ -22,7 +20,6 @@ def getAllRecords():
     listData = list(data)
     json_data = dumps(listData)
     return json_data
-    
     
     
 
@@ -42,16 +39,6 @@ def getSpecificRecords():
     
     
 
-
-#Body
-
-#   {
-#       "id": 2,
-#       "title": "Test 2",
-#       "shortdesc": "test",
-#       "priority": 2
-#   }
-
 @apiHelper_bp.route('/api/addrecord', methods = ['POST'])
 def addRecord():
     recordToInsert = request.json
@@ -61,36 +48,18 @@ def addRecord():
     return getAllRecords()
     
     
-#Body
 
-#   {
-#       "id": 2
-#   }
 @apiHelper_bp.route('/api/delete', methods = ['GET', 'POST'])
 def delete():
     recordsToDelete = request.json
+    recordsToDelete = json.loads(recordsToDelete)
+    print(recordsToDelete)
     objectName = request.headers["objectName"]
     db[objectName].delete_many(recordsToDelete)
-    return getAllRecords()
+    return "{}"
 
 
 
-
-#Body
-
-#   {
-#       "myQuery": {
-#           "id": 10
-#       },
-#       "newValues": {
-#            "$set": {
-#               "id": 1,
-#                "title": "Test 1",
-#                "shortdesc": "test",
-#                "priority": 1
-#           }
-#        }
-#   }
 @apiHelper_bp.route('/api/updaterecord', methods = ['GET', 'POST'])
 def updateRecord():
     requestBody = request.json
@@ -106,7 +75,7 @@ def updateRecord():
     
     db[objectName].update_one(myQuery, newValues)
     
-    return getAllRecords()    
+    return "{}"  
     
     
 if __name__ == '__main__':
